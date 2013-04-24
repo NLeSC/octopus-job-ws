dd36efbd1bfa0c972619088fa69ec918897c5f6d
Stefan Verhoeven <s.verhoeven@esciencecenter.nl>
2013-04-24 16:59:08 +0200

    Fix some PMD warnings.

--- src/main/java/nl/esciencecenter/octopus/webservice/api/JobStatusResponse.java
+++ src/main/java/nl/esciencecenter/octopus/webservice/api/JobStatusResponse.java
@@ -9,9 +9,9 @@ package nl.esciencecenter.octopus.webservice.api;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -95,7 +95,7 @@ public class JobStatusResponse {
 
     @Override
     public String toString() {
-        return com.google.common.base.Objects.toStringHelper(this)
+        return Objects.toStringHelper(this)
                 .addValue(this.state)
                 .addValue(this.done)
                 .addValue(this.exitCode)
--- src/main/java/nl/esciencecenter/octopus/webservice/mac/MacScheme.java
+++ src/main/java/nl/esciencecenter/octopus/webservice/mac/MacScheme.java
@@ -9,9 +9,9 @@ package nl.esciencecenter.octopus.webservice.mac;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -204,7 +204,7 @@ public class MacScheme implements ContextAwareAuthScheme {
      * Date setter
      * @param date
      */
-    public void setDate(java.util.Date date) {
+    public void setDate(Date date) {
         this.date = date;
     }
 
